34f484ba88cce0ee35877fd505dcb3d98eae1f97
Ahmed Ayman <Ahmed.a.elkemary@gmail.com>
2019-06-27 10:29:19 +0200

    Add project files.

---
+++ SETechnicalTask/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SETechnicalTask.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public List<Skill> Skills { get; set; }
+    }
+}
